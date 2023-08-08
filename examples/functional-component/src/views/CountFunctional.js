61d45750a471ad8214dfd0a2568171436ce7575a
Ngan Do <donganngan68@gmail.com>
2023-08-08 09:59:54 +0700

    Update comments

--- examples/functional-component/src/views/CountFunctional.js
+++ examples/functional-component/src/views/CountFunctional.js
@@ -1,22 +1,28 @@
-import Button from '../components/Button';
-import '../style/home.css';
-import { useState } from 'react';
+import Button from '../components/Button'; // Importing a custom Button component
+import '../style/home.css'; // Importing CSS styles for the component
+import { useState } from 'react'; // Importing the useState hook from React
 
+// Defining a functional component named CountFunctional
 const CountFunctional = () => {
+  // Using the useState hook to initialize the count state to 0
   const [count, setCount] = useState(0)
 
+  // Function to increment the count state
   const increament = () => {
     setCount(count + 1)
   }
 
+  // Function to decrement the count state
   const decrease = () => {
     setCount(count - 1)
   }
 
+  // Function to reset the count state to 0
   const restart = () => {
     setCount(0)
   }
 
+  // The return statement defines the component's UI using JSX
   return (
     <div className='main'>
       <div className='container'>
