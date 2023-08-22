4210effaf4e293e9ee95903596382a269c6b864c
Ngan Do <donganngan68@gmail.com>
2023-08-22 15:32:21 +0700

    Update icon app and create .prettierrc.json file

--- practice-one/src/index.js
+++ practice-one/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// Import the necessary libraries/modules
+import React from 'react'; // Import React library
+import ReactDOM from 'react-dom/client'; // Import ReactDOM from the client module
+import App from './App'; // Import the main App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
