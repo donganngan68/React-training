3a22e4a21c0ea4722150dd60c60b77171df470f9
donganngan68 <donganngan68@gmail.com>
2023-08-23 22:07:25 +0700

    Update buttons theme

--- practice-one/src/theme/components/buttons.js
+++ practice-one/src/theme/components/buttons.js
@@ -1,25 +1,33 @@
 export default {
   baseStyle: {
     borderRadius: 0,
+    fontWeight: '400',
   },
   variants: {
     solid: {
       backgroundColor: 'primary',
       color: 'white',
-      h: '3.125rem', // 50px
       px: '1.625rem', // 26px
       _hover: {
         backgroundColor: 'primary',
       },
     },
     outline: {
-      backgroundColor: 'primary',
-      color: 'white',
-      h: '1.688rem', // 27px
+      borderColor: 'primary',
+      color: 'primary',
       px: '1.625rem', // 26px
       _hover: {
+        color: 'white',
         backgroundColor: 'primary',
       },
+    },
+  },
+  sizes: {
+    md: {
+      h: '3.125rem', // 50px
+    },
+    sm: {
+      h: '1.688rem', // 27px
     },
   },
 };
