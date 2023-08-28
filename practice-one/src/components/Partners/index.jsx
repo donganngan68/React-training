06ca7aec988164301076b55da89d85aa97fe02ff
donganngan68 <donganngan68@gmail.com>
2023-08-28 17:22:45 +0700

    Update invalid image for partners component

--- practice-one/src/components/Partners/index.jsx
+++ practice-one/src/components/Partners/index.jsx
@@ -9,9 +9,9 @@ import constants from '../../constants';
 
 export default function Partners() {
   return (
-    <Box>
+    <Box pos="relative">
       <Container>
-        <Flex pt="71" pb="21" justify-content="center">
+        <Flex pos="absolute" pt="71" pb="21" justify-content="center">
           <HStack px="14">
             {constants.partners.map(({ logo, id, name }) => {
               if (!logo) {
