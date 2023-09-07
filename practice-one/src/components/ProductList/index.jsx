ff02d4f16052ed06c7fe49bdd4655e4911155e70
donganngan68 <donganngan68@gmail.com>
2023-09-07 08:50:20 +0700

    Update form validation

--- practice-one/src/components/ProductList/index.jsx
+++ practice-one/src/components/ProductList/index.jsx
@@ -1,77 +1,96 @@
 import React, { useState } from 'react';
+
 import {
-  Box, Button, Input, Text, Flex, Grid, useDisclosure, Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Textarea,
+  Box, Button, Input, Text,
+  Flex, Grid, useDisclosure, Modal, ModalOverlay,
+  ModalContent, ModalHeader, ModalFooter, ModalBody,
+  ModalCloseButton, FormControl, FormLabel, Textarea, FormErrorMessage,
 } from '@chakra-ui/react';
+
 import PropTypes from 'prop-types';
 
 // ProductCard component
 import { ProductCard } from '../ProductCard';
 
+// ApiRequest function
 import { apiRequest } from '../../helpers';
 
-// eslint-disable-next-line no-unused-vars
 export const ProductList = ({ products, submit }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [formProduct, setFormProduct] = useState({
+  const initialFormProduct = {
     title: '',
     price: '',
+    description: '',
     image: '',
-  });
-
+  };
+  const [formProduct, setFormProduct] = useState(initialFormProduct);
+  const [formErrors, setFormErrors] = useState({});
   const handleChange = (e) => {
+    const { name, value } = e.target;
+    // Clear the error message for the current field when it's changed
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: '',
+    }));
     setFormProduct({
       ...formProduct,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   };
 
-  console.log(formProduct.image);
-
-  const submitForm = () => {
-    const getData = async () => {
+  const validateForm = () => {
+    const errors = {};
+    const {
+      title, price, description, image,
+    } = formProduct;
+    if (!title) errors.title = 'Product name is required.';
+    if (!price) errors.price = 'Product price is required.';
+    if (!description) errors.description = 'Product description is required.';
+    if (!image) errors.image = 'Product image is required.';
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0; // Form is valid if errors object is empty
+  };
+  const handleCloseModal = () => {
+    onClose();
+    setFormProduct(initialFormProduct);
+    setFormErrors({});
+  };
+  const submitForm = async () => {
+    if (validateForm()) {
       const newForm = {
         ...formProduct,
-        price: Number(formProduct.price),
         image: 'https://firebasestorage.googleapis.com/v0/b/card-f9463.appspot.com/o/coat.png?alt=media&token=f0930459-c12d-4cda-892a-c2f441df6541',
       };
-
       await apiRequest({ method: 'POST', body: newForm, apiName: 'products' });
-    };
-    getData();
-
-    setFormProduct({
-      title: '',
-      price: '',
-      image: '',
-    });
-
-    onClose();
-    submit();
+      setFormProduct({
+        title: '',
+        price: '',
+        description: '',
+        image: '',
+      });
+      handleCloseModal();
+      submit();
+    }
   };
-
   const handleDeleteRow = () => {
     submit();
   };
-
   return (
     <>
       <Box mx="36" pt="24">
         <Box textAlign="center" pb="10">
-          <Text fontSize="xs" color="primary" fontWeight="semibold" textTransform="uppercase">Top Selling</Text>
-          <Text fontSize="2md" fontWeight="bold">Best Weekend Sellers</Text>
+          <Text fontSize="xs" color="primary" fontWeight="semibold" textTransform="uppercase">
+            Top Selling
+          </Text>
+          <Text fontSize="2md" fontWeight="bold">
+            Best Weekend Sellers
+          </Text>
         </Box>
         <Flex justify="space-between" pb="10">
           <Input variant="filled" w="96" size="lg" placeholder="Searching" />
-          <Button variant="solid" onClick={onOpen}>Add New Product</Button>
+          <Button variant="solid" onClick={onOpen}>
+            Add New Product
+          </Button>
         </Flex>
         <Grid templateColumns="repeat(4, 1fr)" gridColumnGap="5" gridRowGap="6">
           {products.map((product) => (
@@ -79,73 +98,54 @@ export const ProductList = ({ products, submit }) => {
           ))}
         </Grid>
       </Box>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={handleCloseModal}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Add New Product</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <FormControl>
+            <FormControl isInvalid={!!formErrors.title}>
               <FormLabel>Product Name</FormLabel>
-              <Input
-                name="title"
-                value={formProduct.title}
-                onChange={handleChange}
-                placeholder="Product name"
-              />
+              <Input name="title" value={formProduct.title} onChange={handleChange} placeholder="Product name" />
+              <FormErrorMessage>{formErrors.title}</FormErrorMessage>
             </FormControl>
-
-            <FormControl mt={4}>
+            <FormControl mt={4} isInvalid={!!formErrors.price}>
               <FormLabel>Product Price</FormLabel>
-              <Input
-                type="number"
-                name="price"
-                value={formProduct.price}
-                onChange={handleChange}
-                placeholder="Product price"
-              />
+              <Input type="number" name="price" value={formProduct.price} onChange={handleChange} placeholder="Product price" />
+              <FormErrorMessage>{formErrors.price}</FormErrorMessage>
             </FormControl>
-
-            <FormControl mt={4}>
+            <FormControl mt={4} isInvalid={!!formErrors.description}>
               <FormLabel>Product Description</FormLabel>
-              <Textarea
-                placeholder="Product description"
-              />
+              <Textarea placeholder="Product description" name="description" value={formProduct.description} onChange={handleChange} />
+              <FormErrorMessage>{formErrors.description}</FormErrorMessage>
             </FormControl>
-
-            <FormControl mt={4}>
+            <FormControl mt={4} isInvalid={!!formErrors.image}>
               <FormLabel>Product Image</FormLabel>
-              <Input
-                name="image"
-                value={formProduct.image}
-                onChange={handleChange}
-                placeholder="Product image"
-              />
+              <Input name="image" value={formProduct.image} onChange={handleChange} placeholder="Product image" />
+              <FormErrorMessage>{formErrors.image}</FormErrorMessage>
             </FormControl>
           </ModalBody>
-
           <ModalFooter>
             <Button h="10" mr={3} onClick={submitForm}>
               Confirm
             </Button>
-            <Button h="10" onClick={onClose}>Cancel</Button>
+            <Button h="10" onClick={handleCloseModal}>
+              Cancel
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
   );
 };
-
 ProductList.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    image: PropTypes.string,
-    title: PropTypes.string,
-    price: PropTypes.number,
-  })).isRequired,
-};
-
-ProductList.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      image: PropTypes.string,
+      title: PropTypes.string,
+      price: PropTypes.number,
+    }),
+  ).isRequired,
   submit: PropTypes.func.isRequired,
 };
